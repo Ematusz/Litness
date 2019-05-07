5b19c93f4e5a40fbb9b1c7e4f4de47e9692aedff
Evan Matusz <Evan@Evans-MacBook-Pro-2.local>
2019-05-06 21:51:05 -0400

    push with partially completed firestore updates

--- functions/index.js
+++ functions/index.js
@@ -17,19 +17,33 @@ exports.DBupdate = functions.https.onRequest((req, res) => {
     var stuff = [];
     ref.collection('locations').get().then(snapshot => {
         snapshot.forEach(doc => {
+            var twoHoursAgo = Date.now() + 1 * 60 * 60 * 1000;
+            
+            ref.collection('locations').doc(doc.id).collection('votes').where("vote", "=", 1).get().then(snapshot => {
+                snapshot.forEach( vote => {
+                    console.log('succesfully deleted', vote.id, " ", twoHoursAgo);
+                    ref.collection('locations').doc(doc.id).collection('votes').doc(vote.id).delete();
+                })
+                return "";
+            }).catch(reason => {
+                res.send(reason)
+            });
+
             var newelement = {
                 "id": doc.id,
-                "count": math.floor(doc.data().count-doc.data().count*0.2),
-                "timeCreated": doc.data().timeCreated
+                // "count": math.floor(doc.data().count-doc.data().count*0.2),
+                "timeCreated": doc.data().timeCreated,
+                "votes": votes_
             }
             stuff = stuff.concat(newelement);
-            if (newelement.count > 0) {
-                ref.collection('locations').doc(newelement.id).update({
-                    count: newelement.count
-                });
-            } else {
-                ref.collection('locations').doc(newelement.id).delete();
-            }
+            // if (newelement.count > 0) {
+            //     ref.collection('locations').doc(newelement.id).update({
+            //         count: newelement.count
+            //     });
+            // } else {
+            //     ref.collection('locations').doc(newelement.id).delete();
+            // }
+
         });
         res.send(stuff)
         return "";
@@ -61,16 +75,21 @@ exports.replenishCounts = functions.https.onRequest((req, res) => {
 
 exports.updatedVote = functions.firestore.document('locations/{address}/votes/{voterID}')
     .onWrite((change,context) => {
-        if (change.type !== 'delete') {
+        // if (change.type !== 'delete') {
             //value of the new vote for this location
-            var newVote = change.after.data().vote;
+            var newVote = null;
             var oldVote = null;
             // Updates the old vote for this location
             // should ouptut an error if it stays null. Need to learn how
             if (change.before.data() === undefined) {
                 oldVote = 0;
+                newVote = change.after.data().vote;
+            } else if (change.after.data() === undefined) {
+                oldVote = change.before.data().vote;
+                newVote = 0;
             } else {
                 oldVote = change.before.data().vote;
+                newVote = change.after.data().vote;
             }
             //reference to the current location
             var locationRef = ref.collection('locations').doc(context.params.address);
@@ -109,9 +128,39 @@ exports.updatedVote = functions.firestore.document('locations/{address}/votes/{v
                     });
                 });
             });
-        }
-        else {
-            return false;
-        }
+        // }
     });
+
+    // exports.deleteVote = functions.firestore.document('locations/{address}/votes/{voterID}')
+    //     .onDelete((change,context) => {
+    //         var deletedVote = change.before.data().vote;
+    //         var locationRef = ref.collection('locations').doc(context.params.address);
+    //         return ref.runTransaction(transaction => {
+    //             return transaction.get(locationRef).then(locationDoc => {
+    //                 //compute new count
+    //                 var currentCount = locationDoc.data().count - deletedVote;
+    //                 //compute upVotes if the new vote is an up vote
+    //                 var upVotes_ = locationDoc.data().upVotes;
+    //                 if(deletedVote === 1) {
+    //                     upVotes_ -= 1;
+    //                 }
+    //                 //compute downVotes
+    //                 var downVotes_ = locationDoc.data().downVotes;
+    //                 if(deletedVote === -1) {
+    //                     downVotes_ -= 1;
+    //                 }
+    //                 //compute percentVotesLastThirty
+
+    //                 //compute percentVotesLastHour
+
+    //                 //update location info
+    //                 console.log("update successful");
+    //                 return transaction.update(locationRef, {
+    //                     count: currentCount,
+    //                     upVotes: upVotes_,
+    //                     downVotes: downVotes_,
+    //                 });
+    //             });
+    //         });
+    //     })
     
