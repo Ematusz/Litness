bc81822354a35ad9a5031811b36b5cd8397d1737
whitelighting24 <mevanm@umich.edu>
2019-09-03 18:01:11 -0400

    added an interstital ad on initial load of the app and a banner ad to the top

---
+++ InterstitialAd.js
@@ -0,0 +1,34 @@
+import { AdMobBanner } from "expo-ads-admob";
+import React from 'react';
+import {TouchableOpacity,View,Text} from 'react-native';
+import styles from './styles.js'
+
+export default class InerstitialAd extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+
+        this.bannerError = this.bannerError.bind(this);
+    }
+
+    bannerError() {
+        console.log("Error occured")
+    }
+    
+    render() {
+        return(
+            <View style = {{width: "100%", height: 100, position: 'absolute', top: 0}}> 
+                <AdMobBanner
+                    style={styles.adMobBanner}
+                    bannerSize="largeBanner"
+                    adUnitID="ca-app-pub-3940256099942544/6300978111"
+                    testDeviceID="EMULATOR"
+                    didFailToReceiveAdWithError={this.bannerError}
+                />
+            </View>
+            
+        )
+    }
+}
