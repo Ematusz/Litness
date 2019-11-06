2884c1ea9b64964dfcd16feeb3c7f855bc62ad41
whitelighting24 <mevanm@umich.edu>
2019-11-06 16:24:49 -0500

    implemented AdOptionsView but realized this is still a really buggy library so still didnt turn it on for development.

--- FacebookNativeAd.js
+++ FacebookNativeAd.js
@@ -4,14 +4,17 @@ import {View,Text} from 'react-native';
 import styles from './styles.js';
 import Dimensions from 'Dimensions';
 
-const { AdTriggerView, AdIconView, AdMediaView } = FacebookAds;
+const { AdTriggerView, AdIconView, AdMediaView, AdOptionsView } = FacebookAds;
 
 class FacebookNativeAd extends React.Component {
     render() {
         return (
             <View style={{...styles.leaderBoardCell, paddingBottom: '1.5%', backgroundColor: '#d3d3d3'}}>
-                <AdMediaView style={{height:1,width:1}}/>
-                <AdIconView style={styles.emojiIcon} />
+                <View style = {{flexDirection: 'column'}}>
+                    <AdMediaView style={{height:1,width:1}}/>
+                    <AdOptionsView/>
+                    <AdIconView style={styles.emojiIcon} />
+                </View>
                 <View style = {{display:'flex', flexDirection:'column',paddingHorizontal: Dimensions.get('window').width*0.0242, width: Dimensions.get('window').width*0.546}}>
                     <Text style={{fontSize: Dimensions.get('window').width*0.0242}}>{this.props.nativeAd.sponsoredTranslation}</Text>
                     <Text style={{fontSize: Dimensions.get('window').width*0.0483}}>{this.props.nativeAd.advertiserName}</Text>
